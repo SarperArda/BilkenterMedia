f9a6d58d67ffe30221ac5ebe3423a9dfd0948292
Sarper Arda Bakır <arda.bakir@ug.bilkent.edu.tr>
2022-04-18 14:27:12 +0300

    Binding added

--- Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/Blog.java
+++ Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/Blog.java
@@ -3,12 +3,17 @@ package com.thenameless.bilkenter_media;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.thenameless.bilkenter_media.databinding.ActivityBlogBinding;
 
 public class Blog extends AppCompatActivity {
-
+    private ActivityBlogBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_blog);
+        binding = ActivityBlogBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
--- Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/Profile.java
+++ Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/Profile.java
@@ -3,12 +3,18 @@ package com.thenameless.bilkenter_media;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.thenameless.bilkenter_media.databinding.ActivityHomeBinding;
+import com.thenameless.bilkenter_media.databinding.ActivityProfileBinding;
 
 public class Profile extends AppCompatActivity {
-
+    private ActivityProfileBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
