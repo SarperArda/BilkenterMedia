2f936decb3e6760834de4c70800109ecea1989c8
Sarper Arda Bakır <arda.bakir@ug.bilkent.edu.tr>
2022-04-19 11:01:50 +0300

    Profile editted and settings added

--- Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/Profile.java
+++ Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/Profile.java
@@ -2,6 +2,7 @@ package com.thenameless.bilkenter_media;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,5 +17,11 @@ public class Profile extends AppCompatActivity {
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+    }
+
+    public void goToSetting(View view){
+        Intent intent = new Intent(Profile.this,Settings.class);
+        startActivity(intent);
+        finish();
     }
 }
