75a12d7e28caf7df4e2f9055fc70e2c73e046e21
kaanozer7 <kaan.ozer@ug.bilkent.edu.tr>
2022-05-06 23:43:21 +0300

    editing account is half, deleting account is added

--- Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/EditProfile.java
+++ Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/EditProfile.java
@@ -2,7 +2,6 @@ package com.thenameless.bilkenter_media;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -10,6 +9,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,35 +20,55 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EditProfile extends AppCompatActivity {
 
-    Button changePicture, resetPassword, goBackToProfile, saveProfile;
+    private static final String TAG = "TAG";
+    Button changePicture, saveProfile;
     FirebaseFirestore fStore;
     FirebaseAuth fAuth;
     String userID;
     FirebaseUser user;
     ImageView profileImage;
     StorageReference fStorage;
+    EditText profileName, profileSurname, profileBirthday, profileMail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        resetPassword = findViewById(R.id.reset);
+        Intent data = getIntent();
+        String name = data.getStringExtra("name");
+        String surname = data.getStringExtra("surname");
+        String birthday = data.getStringExtra("birthday");
+        String mail = data.getStringExtra("e-mail");
 
+        profileName = findViewById(R.id.PersonName);
+        profileSurname = findViewById(R.id.PersonSurname);
+        profileBirthday = findViewById(R.id.PersonBirthday);
+        profileMail = findViewById(R.id.personMail);
         profileImage = findViewById(R.id.profileImage);
+
         changePicture = findViewById(R.id.changePicture);
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
         fStorage = FirebaseStorage.getInstance().getReference();
+        userID = fAuth.getCurrentUser().getUid();
+        user = fAuth.getCurrentUser();
+
+
+        saveProfile = findViewById(R.id.saveBtn);
 
         StorageReference profileRef = fStorage.child("Users Database/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
         profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
@@ -58,49 +78,55 @@ public class EditProfile extends AppCompatActivity {
             }
         });
 
-        userID = fAuth.getCurrentUser().getUid();
-        user = fAuth.getCurrentUser();
-
-        resetPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText resetPassword = new EditText(v.getContext());
-                AlertDialog.Builder resetDialog = new AlertDialog.Builder(v.getContext());
-                resetDialog.setTitle("Reset Password?");
-                resetDialog.setMessage("Enter New Password");
-                resetDialog.setView(resetPassword);
-
-                resetDialog.setPositiveButton("Done", (dialog, which)  -> {
-                    String newPassword = resetPassword.getText().toString();
-                    user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            Toast.makeText(EditProfile.this, "Password Reset Successfully!", Toast.LENGTH_SHORT).show();
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(EditProfile.this, "Password Reset Failed", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                });
-
-                resetDialog.setNegativeButton("Cancel", (dialog, which) ->{
-                });
-                resetDialog.create().show();
-            }
-        });
-
         changePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //opening the gallery
                 Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(openGalleryIntent, 1000);
             }
         });
 
+        saveProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(profileName.getText().toString().isEmpty() ||profileSurname.getText().toString().isEmpty() || profileBirthday.getText().toString().isEmpty()){
+                    Toast.makeText(EditProfile.this, "One or many fields are empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String email = profileMail.getText().toString();
+                user.updateEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        DocumentReference reference = fStore.collection("Users Database").document(user.getUid());
+                        Map<String,Object> edited = new HashMap<>();
+                        edited.put("Name",profileName);
+                        edited.put("Surname",profileSurname);
+                        edited.put("Birthday",profileBirthday);
+                        edited.put("E-mail",email);
+                        reference.update(edited).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(EditProfile.this, "Profile Updated", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), Profile.class));
+                                finish();
+                            }
+                        });
+                        Toast.makeText(EditProfile.this, "Changed Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(EditProfile.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        profileMail.setText(mail);
+        profileName.setText(name);
+        profileSurname.setText(surname);
+        profileBirthday.setText(birthday);
 
+        Log.d(TAG, "onCreate: "+name+ " "+ surname+" "+mail+" "+ birthday );
     }
 
     @Override
--- Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/Profile.java
+++ Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/Profile.java
@@ -1,29 +1,44 @@
 package com.thenameless.bilkenter_media;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.thenameless.bilkenter_media.databinding.ActivityHomeBinding;
-import com.thenameless.bilkenter_media.databinding.ActivityProfileBinding;
 
 public class Profile extends AppCompatActivity {
 
     TextView fullName, email, birthday;
-    String name, surname;
     FirebaseAuth mAuth;
     FirebaseFirestore fStore;
+    String name, surname;
     String userID;
+    ImageView profilePicture;
+    Button changePicture, resetPassword, deleteAccount;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,42 +48,113 @@ public class Profile extends AppCompatActivity {
         email = findViewById(R.id.email);
         birthday = findViewById(R.id.birthdayDate);
         fullName = findViewById(R.id.fullName);
-        //name = findViewById(R.id.Username);
-        //surname = findViewById(R.id.UserSurname);
+        profilePicture = findViewById(R.id.profilePicture);
+
+        deleteAccount = findViewById(R.id.deleteAccount);
+
+        resetPassword = findViewById(R.id.reset);
 
         mAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
 
         userID = mAuth.getCurrentUser().getUid();
+        user = mAuth.getCurrentUser();
 
         DocumentReference documentReference = fStore.collection("Users Database").document(userID);
-
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                email.setText(value.getString("E-mail"));
-                birthday.setText(value.getString("Birthday"));
-                name = value.getString("Name");
-                surname = value.getString("Surname");
-                fullName.setText(name + " " + surname);
+                if(value.exists()){
+                    email.setText(value.getString("E-mail"));
+                    birthday.setText(value.getString("Birthday"));
+                    name = value.getString("Name");
+                    surname = value.getString("Surname");
+                    fullName.setText(name + " " + surname);
+                }else{
+                    Log.d("tag", "Document does not exists");
+                }
 
-                //String firstName = value.getString(String.valueOf(name));
-                //String secondName = value.getString(String.valueOf(surname));
-                //fullName.setText(firstName + secondName);
-                //birthday.setText(value.getString("Birthday"));
             }
         });
-    }
+        resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText resetPassword = new EditText(v.getContext());
+                AlertDialog.Builder resetDialog = new AlertDialog.Builder(v.getContext());
+                resetDialog.setTitle("Reset Password?");
+                resetDialog.setMessage("Enter New Password");
+                resetDialog.setView(resetPassword);
 
-    public void goToSetting(View view){
-        Intent intent = new Intent(Profile.this,Settings.class);
-        startActivity(intent);
-        finish();
-    }
-    public void goToBlogs(View view){
-        Intent intent = new Intent(Profile.this, Blog.class);
-        startActivity(intent);
-        finish();
+                resetDialog.setPositiveButton("Done", (dialog, which)  -> {
+                    String newPassword = resetPassword.getText().toString();
+                    user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(Profile.this, "Password Reset Successfully!", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Profile.this, "Password Reset Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                });
+
+                resetDialog.setNegativeButton("Cancel", (dialog, which) ->{
+                });
+                resetDialog.create().show();
+            }
+        });
+
+        deleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(Profile.this);
+                dialog.setTitle("Are you sure?");
+                dialog.setMessage("Deleting this account will result in completely removing your account.");
+                dialog.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(Profile.this, "Account Deleted", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(Profile.this, SignUp.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                }else{
+                                    Toast.makeText(Profile.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                    }
+                });
+                dialog.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
+            }
+        });
+
+        /*changePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //opening the gallery
+                Intent i = new Intent(v.getContext(), EditProfile.class);
+                i.putExtra("Name", name);
+                i.putExtra("Surname", surname);
+                i.putExtra("birthday", birthday.getText().toString());
+                i.putExtra("e-mail", email.getText().toString());
+                startActivity(i);
+            }
+        });*/
     }
     public void goToEditProfile(View view){
         Intent intent = new Intent(Profile.this, EditProfile.class);
@@ -80,4 +166,5 @@ public class Profile extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
         finish();
     }
+
 }
