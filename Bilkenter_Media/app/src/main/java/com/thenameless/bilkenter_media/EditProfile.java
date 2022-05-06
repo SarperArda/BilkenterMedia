d06a66da3e48052eaf1622e987fed1c0dbaa7e35
kaanozer7 <kaan.ozer@ug.bilkent.edu.tr>
2022-05-06 18:49:43 +0300

    profile picture feature added

--- Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/EditProfile.java
+++ Bilkenter_Media/app/src/main/java/com/thenameless/bilkenter_media/EditProfile.java
@@ -1,14 +1,19 @@
 package com.thenameless.bilkenter_media;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -16,6 +21,10 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -24,6 +33,8 @@ public class EditProfile extends AppCompatActivity {
     FirebaseAuth fAuth;
     String userID;
     FirebaseUser user;
+    ImageView profileImage;
+    StorageReference fStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +43,20 @@ public class EditProfile extends AppCompatActivity {
 
         resetPassword = findViewById(R.id.reset);
 
+        profileImage = findViewById(R.id.profileImage);
+        changePicture = findViewById(R.id.changePicture);
+
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
+        fStorage = FirebaseStorage.getInstance().getReference();
+
+        StorageReference profileRef = fStorage.child("Users Database/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(profileImage);
+            }
+        });
 
         userID = fAuth.getCurrentUser().getUid();
         user = fAuth.getCurrentUser();
@@ -65,6 +88,50 @@ public class EditProfile extends AppCompatActivity {
                 resetDialog.setNegativeButton("Cancel", (dialog, which) ->{
                 });
                 resetDialog.create().show();
+            }
+        });
+
+        changePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //opening the gallery
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent, 1000);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+                //profileImage.setImageURI(imageUri);
+                uploadImage(imageUri);
+            }
+        }
+    }
+
+    private void uploadImage(Uri imageUri){
+        StorageReference ref = fStorage.child("Users Database/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        ref.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(profileImage);
+                    }
+                });
+                Toast.makeText(EditProfile.this, "Image Uploaded!", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(EditProfile.this, "Upload Failed", Toast.LENGTH_SHORT).show();
             }
         });
     }
